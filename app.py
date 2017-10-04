e175088a995a7a3203c68d5d6d179d1b292df7b9
Edwin Mendez <emendez@gravitalsoftware.com>
2017-10-04 15:07:52 -0500

    setting

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    print('******************************************')
-    print(os.environ['APP_SETTINGS'])
     return "Hello World!"
 
 
