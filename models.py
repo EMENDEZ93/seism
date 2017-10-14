ab21c3069296407d3e8b06e9a0b8a9176caef136
Edwin Mendez <emendez@gravitalsoftware.com>
2017-10-14 18:11:41 -0500

    Country, Department, City models

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
-
+from sqlalchemy import ForeignKey
 
 class Result(db.Model):
     __tablename__ = 'results'
@@ -14,6 +14,46 @@ class Result(db.Model):
         self.url = url
         self.result_all = result_all
         self.result_no_stop_words = result_no_stop_words
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class Country(db.Model):
+    __tablename__ = 'country'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+
+    def __init__(self, name):
+        self.url = name
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+class Department(db.Model):
+    __tablename__ = 'department'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    country_id = db.Column(db.Integer(), ForeignKey(Country.id))
+
+    def __init__(self, name):
+        self.url = name
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class City(db.Model):
+    __tablename__ = 'city'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    department_id = db.Column(db.Integer(), ForeignKey(Department.id))
+
+    def __init__(self, name):
+        self.url = name
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
